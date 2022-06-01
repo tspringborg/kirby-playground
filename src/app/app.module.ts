d2ef648068fe033f1b948d31e8ab9397bf90dfbe
Thomas Springborg <thsp@jyskebank.dk>
2022-06-01 13:43:02 +0200

    install kirby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KirbyModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
