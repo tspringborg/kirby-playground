6cb7b1cbe28e5899dccb366245e0bd15ceb39c78
Thomas Springborg <thsp@jyskebank.dk>
2022-06-01 14:19:18 +0200

    kirby working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { KirbyModule } from '@kirbydesign/designsystem';
 
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     KirbyModule,
     FormsModule,
+    RouterModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
